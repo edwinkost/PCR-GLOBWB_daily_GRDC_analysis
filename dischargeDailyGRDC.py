1cf3246c2d7629010ec748de50c7f7718ee67395
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-02 16:04:06 +0100

    Discharge analyzing.

--- dischargeDailyGRDC.py
+++ dischargeDailyGRDC.py
@@ -474,7 +474,7 @@ class DailyDischargeEvaluation(object):
                 
                 # select specific ranges of date/year
                 nctime   = f.variables['time']                                # A netCDF time variable object. 
-                cropTime = nctime[:]
+                cropTime = range(nctime[0], nctime[-1] + 1, 1) 
                 
                 # assume that the netcdf file contains only one year:
                 cropData = cropData[:]
