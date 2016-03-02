3e6d4c9619f5a700deb3821a68ba602dbcfabbee
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-02 18:56:36 +0100

    Discharge analyzing.

--- 0_bash_parallel_discharge_analysis.py
+++ 0_bash_parallel_discharge_analysis.py
@@ -10,26 +10,9 @@ pcrglobwb_output_folder = str(sys.argv[1])
 
 number_of_subfolders = 19
 
-for i in range(1, 5, 1):
+for i in range(1, number_of_subfolders + 1, 1):
     sub_folder = '%02i' %(i)
-    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
-    print cmd
-    os.system(cmd)
- 
-for i in range(6, 11, 1):
-    sub_folder = '%02i' %(i)
-    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
-    print cmd
-    os.system(cmd)
-
-for i in range(11, 16, 1):
-    sub_folder = '%02i' %(i)
-    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
-    print cmd
-    os.system(cmd)
-
-for i in range(16, number_of_subfolders + 1, 1):
-    sub_folder = '%02i' %(i)
-    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
+    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output "
+    if (i % 5 == 0): cmd = cmd + " & "
     print cmd
     os.system(cmd)
