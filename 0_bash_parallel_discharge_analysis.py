7bbdd2b94f436a96dc61847c062da9e93f179410
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-02 18:49:35 +0100

    Discharge analyzing.

--- 0_bash_parallel_discharge_analysis.py
+++ 0_bash_parallel_discharge_analysis.py
@@ -10,10 +10,26 @@ pcrglobwb_output_folder = str(sys.argv[1])
 
 number_of_subfolders = 19
 
-for i in range(1, number_of_subfolders + 1, 1):
+for i in range(1, 5, 1):
     sub_folder = '%02i' %(i)
     cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
     print cmd
     os.system(cmd)
  
-    
+for i in range(6, 11, 1):
+    sub_folder = '%02i' %(i)
+    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
+    print cmd
+    os.system(cmd)
+
+for i in range(11, 16, 1):
+    sub_folder = '%02i' %(i)
+    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
+    print cmd
+    os.system(cmd)
+
+for i in range(16, number_of_subfolders + 1, 1):
+    sub_folder = '%02i' %(i)
+    cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " " + pcrglobwb_output_folder + " discharge_dailyTot_output &"
+    print cmd
+    os.system(cmd)
