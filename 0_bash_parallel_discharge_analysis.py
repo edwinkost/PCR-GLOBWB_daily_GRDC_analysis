5bbdb3721c072c349b63d477ca7f005e9dcc6d27
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-02 14:14:08 +0100

    Discharge analyzing.

--- 0_bash_parallel_discharge_analysis.py
+++ 0_bash_parallel_discharge_analysis.py
@@ -13,5 +13,7 @@ number_of_subfolders = 19
 for i in range(1, number_of_subfolders + 1, 1):
     sub_folder = '%02i' %(i)
     cmd = 'python 0_main_analyze_discharge.py '+ sub_folder + " discharge_dailyTot_output &"
-    print cmd 
+    print cmd
+    os.system(cmd)
+ 
     
