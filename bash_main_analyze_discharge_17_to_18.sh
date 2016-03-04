06a68ee68c450b459abfe2c2f279f54b1ee08a5f
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-04 18:55:38 +0100

    Finishing the unfinished business.

---
+++ bash_main_analyze_discharge_17_to_18.sh
@@ -0,0 +1,4 @@
+
+python 0_main_analyze_discharge.py 17 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_4LCs_edwin_parameter_set_kinematic_wave/daily/ discharge_dailyTot_output &
+python 0_main_analyze_discharge.py 18 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_4LCs_edwin_parameter_set_kinematic_wave/daily/ discharge_dailyTot_output &
+wait
